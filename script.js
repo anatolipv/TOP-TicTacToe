44b010f7b6027cfc7be79b5958496bdc6fdb7bf4
anatolipv <anatoli.vachev@gmail.com>
2022-04-06 18:02:03 +0300

    func update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const game={
     winningCombinations:[
         // Rows
         ["0","1","2"],
-        ["3","4","6"],
+        ["3","4","5"],
         ["6","7","8"],
         // Columns
         ["0","3","6"],
@@ -15,7 +15,6 @@ const game={
         ["0","4","8"],
         ["2","4","6"]
     ]
-
 }
 
 document.addEventListener("click",event =>{
@@ -25,8 +24,6 @@ document.addEventListener("click",event =>{
 
     if(isCell && !isDisabled){
         const cellValue = target.dataset.value;
-
-        // game.xTurn  == true ? game.xState.push(cellValue):game.oState.push(cellValue);
         if(game.xTurn){
             game.xState.push(cellValue);
             target.classList.add("x");
@@ -35,15 +32,9 @@ document.addEventListener("click",event =>{
             game.oState.push(cellValue);
             target.classList.add("o");
         }
-
-        target.classList.add("disabled");
-        
+        target.classList.add("disabled");   
         game.xTurn=!game.xTurn;
-
         checkResult();
-
-      
-       
     }
 });
 
@@ -52,25 +43,40 @@ document.addEventListener("click",event =>{
 
 
 function checkResult(){
-    game.winningCombinations.forEach(winningCombinations => {
-        const xWins = winningCombinations.every(state => game.xState.includes(state))
-        const oWins = winningCombinations.every(state => game.oState.includes(state))
-    
-        if (xWins || oWins) {
-            document.querySelectorAll('.cell').forEach(cell => cell.classList.add('disabled'))
-            document.querySelector('.game-over').classList.add('visible')
-            document.querySelector('.game-over-text').textContent = xWins
-                ? 'X wins!'
-                : 'O wins!'
-        }
-        else if(document.querySelectorAll(".disabled").length==9){
-            document.querySelector('.game-over').classList.add('visible');
-            document.querySelector('.game-over-text').textContent = 'Draw!';
-        }
-        
-    })  
+    const gameOver =  document.querySelector('.game-over');
+    const gameMsg = document.querySelector('.game-over-text');
 
-}
+    if(document.querySelectorAll(".disabled").length==9 ){
+        gameOver.classList.add('visible');
+        gameMsg.textContent = 'Draw!';
+     }
+    game.winningCombinations.forEach(winningCombinations => {
+        const xWins = winningCombinations.every(state => game.xState.includes(state));
+        const oWins = winningCombinations.every(state => game.oState.includes(state));
+        
+        if (xWins || oWins) {
+            gameOver.classList.add('visible');
+            if(xWins)
+                gameMsg.textContent = "X WINS";
+            else 
+                gameMsg.textContent = "O WINS";
+        }      
+         });
+    document.querySelector(".restart").addEventListener("click",()=>{
+        game.xTurn=true;
+        game.xState=[];
+        game.oState=[];
+        document.querySelector('.game-over').classList.remove("visible");
+       
+        document.querySelectorAll(".cell").forEach(cell=>{
+            cell.classList.remove("disabled","x","o");
+        });
+
+    });
+};
+
+   
+    
 
 
 
